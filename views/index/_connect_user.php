22237d0f8b21e9c52dafcd678cfefcb60f08bb44
tgloeggl <tgloeggl@uos.de>
2019-07-25 10:58:30 +0200

    fix utf-8

--- views/index/_connect_user.php
+++ views/index/_connect_user.php
@@ -1,11 +1,11 @@
-<h2>Mit vorhandenem Nutzer in Moodle verknüpfen</h2>
+<h2>Mit vorhandenem Nutzer in Moodle verknÃ¼pfen</h2>
 <form method="post" action="<?= $controller->url_for('index/connect_user/') ?>">
     <?= CSRFProtection::tokenTag() ?>
-    Moodle-User der verknüpft werden soll:<br> 
-    <input name="moodle_username">    
+    Moodle-User der verknÃ¼pft werden soll:<br>
+    <input name="moodle_username">
     </input><br>
-    Moodle-Passwort des Moodle-Users der verknüpft werden soll:<br>
+    Moodle-Passwort des Moodle-Users der verknÃ¼pft werden soll:<br>
     <input name="moodle_user_pw">
     </input><br>
-    <?= \Studip\Button::create(_('Mit Nutzer verknüpfen')) ?>
+    <?= \Studip\Button::create(_('Mit Nutzer verknÃ¼pfen')) ?>
 </form>
