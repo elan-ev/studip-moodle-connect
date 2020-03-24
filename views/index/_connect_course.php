928d7b15fcd672dfe3362d90dd0482c72d82e0b2
David Siegfried <ds.siegfried@gmail.com>
2020-03-24 21:25:10 +0100

    cleanup

--- views/index/_connect_course.php
+++ views/index/_connect_course.php
@@ -1,12 +1,12 @@
-<h2>Mit Moodlekurs verknüpfen</h2>
-<form method="post" action="<?= $controller->url_for('index/connect/') ?>">
+<h2><?= dgettext('moodle_connect', 'Mit Moodlekurs verknüpfen') ?></h2>
+<form method="post" action="<?= $controller->url_for('index/connect/') ?>" class="default">
     <?= CSRFProtection::tokenTag() ?>
 
     <select name="moodle_course">
-        <? foreach($moodle_courses as $course) : ?>
-        <? if ($course['format'] != 'site') : /* skip the installation itself, which is represented as a course as well */ ?>
-            <option value="<?= $course['id'] ?>"><?= htmlReady($course['fullname'] )?></option>
-        <? endif ?>
+        <? foreach ($moodle_courses as $course) : ?>
+            <? if ($course['format'] != 'site') : /* skip the installation itself, which is represented as a course as well */ ?>
+                <option value="<?= $course['id'] ?>"><?= htmlReady($course['fullname']) ?></option>
+            <? endif ?>
         <? endforeach ?>
     </select>
     <?= \Studip\Button::create(_('Mit Moodlekurs verknüpfen')) ?>
--- views/index/_connect_user.php
+++ views/index/_connect_user.php
@@ -1,11 +1,14 @@
-<h2>Mit vorhandenem Nutzer in Moodle verknüpfen</h2>
-<form method="post" action="<?= $controller->url_for('index/connect_user/') ?>">
+<h2><?= dgettext('moodle_connect', 'Mit vorhandenem Nutzer in Moodle verknüpfen') ?></h2>
+<form method="post" action="<?= $controller->url_for('index/connect_user/') ?>" class="default">
     <?= CSRFProtection::tokenTag() ?>
-    Moodle-User der verknüpft werden soll:<br>
-    <input name="moodle_username">
-    <br>
-    Moodle-Passwort des Moodle-Users der verknüpft werden soll:<br>
-    <input name="moodle_user_pw">
-    <br>
+    <label>
+        <?= dgettext('moodle_connect', 'Moodle-User der verknüpft werden soll:') ?><br>
+        <input name="moodle_username">
+    </label>
+    <label>
+
+        <?= dgettext('moodle_connect', 'Moodle-Passwort des Moodle-Users der verknüpft werden soll:') ?>
+        <input name="moodle_user_pw">
+    </label>
     <?= \Studip\Button::create(_('Mit Nutzer verknüpfen')) ?>
 </form>
